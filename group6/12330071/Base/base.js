ff0fb19d5234978e64d1a76108a9a16b10ca51eb
dengyh <dengyh071@gmail.com>
2015-03-20 22:59:32 +0800

    Complete the homework1 and parts of homework2

---
+++ group6/12330071/Base/base.js
@@ -0,0 +1,40 @@
+var Base = function (variable) {
+  this.instanceVariable = variable;
+  this.name = 'haha';
+  this.instanceMethod = function () {
+    console.log('This is from Base class instance-method, static-variable is: ' + this.instanceVariable);
+  };
+};
+Base.staticVariable = 'Base';
+Base.staticMethod = function () {
+  console.log('This is from Base class static-method, static-variable is: ' + this.staticVariable);
+};
+
+function extend(base, derived) {
+  derived = function (variable) {
+    this.instanceVariable = variable;
+    this.instanceMethod = function () {
+      derived.prototype.instanceMethod.call(this);
+      console.log('This is from Derived class instance-method, instance-variable is: ' + this.instanceVariable);
+    };
+  };
+  derived.prototype = new base();
+  derived.staticVariable = 'Derived';
+  derived.staticMethod = function () {
+    base.staticMethod.call(this);
+    console.log('This is from Derived class static-method, static-variable is: ' + this.staticVariable);
+  };
+  return derived;
+}
+
+// var base = new Base();
+// base.constructor.staticMethod();
+
+var Derived;
+Derived = extend(Base, Derived);
+
+var example = new Derived('example');
+var otherExample = new Derived('other-example');
+Derived.staticMethod();
+example.instanceMethod();
+otherExample.instanceMethod();
