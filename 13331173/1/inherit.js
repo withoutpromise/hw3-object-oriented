741771a8ff01ca05c4e5dd6434c23dfdeb1146f5
lmoavne <642148322@qq.com>
2015-03-21 18:47:38 +0800

    liu
    V4

--- 13331173/1/inherit.js
+++ 13331173/1/inherit.js
@@ -1,35 +1,3 @@
-var Base = function (insV) {
-	var staticVarible = 'Base';
-	var instanceVariable = insV;
-	var staticMethod = function () {
-		alert("This is from Base class static-method, static-variable is: " + staticVarible);
-	};
-	var instanceMethod = function () {
-		alert("This is from Base class static-method, static-variable is: " + instanceVariable);
-	};
-};
-
-var Derived = function (insV) {};
-
-function extend(Base, Derived) {
-	Derived.staticVarible = 'Derived';
-	Derived.instanceVariable = Derived.arguments[0];
-	Derived.staticMethod = function () {
-		Base.staticMethod();
-		alert("This is from Derived class static-method, static-variable is: " + staticVariable);
-	};
-	Derived.instanceMethod = function () {
-		Base.instanceMethod();
-		alert("This is from Derived class instance-method, instance-variable is: " + instanceVariable);
-	}
-};
-
-
-
-
-
-
-
 (function () {
 	var Base = function (insV) {
 		this.instanceVariable = insV;
