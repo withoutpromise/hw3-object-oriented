af5d3d9f1d088810ec15325ae677f97683a3330b
StargazerYi <yichuanxiong@126.com>
2015-03-21 18:11:27 +0800

    allsubmitted

---
+++ Group9/12330340/2, 3/2.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+    var tables = getAllTables();
+    makeAllTablesFilterable(tables);
+}
+
+/** Get the content of the table **/
+function getAllTables() {
+    tables = document.getElementsByTagName("table");
+    return tables;
+}
+
+function makeAllTablesFilterable(tables) {
+    var tableRow = [];
+    for (var i = 0; i < tables.length; i++) {
+    	tableRow[i] = tables[i].getElementsByTagName("tr");
+    }
+
+    var button = document.getElementsByTagName("button");
+    var input = document.getElementsByTagName("input");
+
+    for (var i = 0; i < button.length; i++) {
+        button[i].onclick = function(i) {
+            return function() {
+                var str = input[i].value;
+                Filter(tableRow[i], str, tables[i]);
+            }
+        }(i);
+    }
+}
+
+function Filter(rows, str, table) {
+    var removed = [];
+    for (var i = 1; i < rows.length; i++) {
+        var match = 0;
+        text = rows[i].getElementsByTagName("td");
+        for (var j = 0; j < text.length; j++) {
+            subStr = text[j].innerHTML.split(str);
+            if (subStr.length > 1) {
+                match += (subStr.length - 1);
+                subStr[0] += "<span>" + str;
+                subStr[subStr.length - 1] = "</span>" + subStr[subStr.length - 1];
+            }
+                
+            if (subStr.length > 2) {
+                for (var m = 1; m < subStr.length-1; m++) {
+                    subStr[m] = "</span>" + subStr[m] + "<span>" + str;
+                }
+            }
+            text[j].innerHTML = "";
+            for (var m = 0; m < subStr.length; m++)
+                text[j].innerHTML += subStr[m];
+        }
+
+        if (match == 0) {
+            removed.push(i);
+        }
+    }
+
+    for (var i = 0; i < removed.length; i++) {
+        if  (i != 0)
+            table.getElementsByTagName("tbody")[0].removeChild(rows[i-1]);
+        else
+            table.getElementsByTagName("tbody")[0].removeChild(rows[i]);
+    }
+}
