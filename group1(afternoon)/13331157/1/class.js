c00a1c54a28cdaabfc9e5669ad3ef5b293096451
linytSysu <linytSysu@163.com>
2015-03-21 17:01:40 +0800

    asdf

---
+++ group1(afternoon)/13331157/1/class.js
@@ -0,0 +1,72 @@
+window.onload = function() {
+    Derived = extend(Base, Derived);
+
+    example = new Derived('example');
+    Derived.staticMethod();
+    example.instanceMethod();
+
+    console.log("----------------------------\n");
+
+    example = new Derived('example');
+	otherExample = new Derived('other-example');
+	Derived.staticMethod();
+	example.instanceMethod();
+	otherExample.instanceMethod();
+
+};
+
+/**
+ * Base
+ */
+function Base(instanceVariable) {
+    this.instanceVariable = instanceVariable;    //实例变量
+}
+Base.staticVariable = 'Base';                    //类变量
+Base.staticMethod = function() {                 //类方法
+    console.log("This is from Base class static-method, static-variable is: " + this.staticVariable);
+}
+Base.prototype.instanceMethod = function() {     //实例方法
+    console.log("This is from Base class instance-method, instance-variable is: " + this.instanceVariable);
+}
+
+/**
+ * Derived
+ */
+function Derived(instanceVariable) {
+	this.instanceVariable = instanceVariable;         //实例变量
+}
+Derived.staticVariable = 'Derived';                   //类变量
+Derived.staticMethod = function() {                   //类方法
+	console.log("This is from Derived class static-method, static-variable is: " + this.staticVariable);
+}
+Derived.prototype.instanceMethod = function() {       //实例方法
+	console.log("This is from Derived class instance-method, instance-variable is: " + this.instanceVariable);
+}
+
+/*
+ * extend方法
+ */
+function extend(base, derived) {
+	var Derived = function() {     // 获取base, derived的实例变量
+		base.apply(this, arguments);
+		derived.apply(this, arguments);
+	}
+	inher(Derived, base, derived);   // 获取derived的类变量, base, derived的类方法
+	inher(Derived.prototype, base.prototype, derived.prototype);  // 获取base, derived的实例方法
+    return Derived;
+};
+function inher(obj, base, derived) {
+	for (var key in derived) {
+        obj[key] = derived[key];
+	}
+    for (var key in base) {
+        if (typeof base[key] === 'function') {
+        	if (typeof obj[key] === 'function') {
+	        	obj[key] = function() {
+	        		base[key].apply(this, arguments);
+	                derived[key].apply(this, arguments);
+	        	}
+            }
+        }
+    }
+}
---
+++ group1(afternoon)/13331157/2/filter.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+    var tables = getAllTables();
+    makeAllTablesFilterable(tables);
+};
+
+function getAllTables() {
+    return document.getElementsByTagName('table');
+};
+
+function makeAllTablesFilterable(tables) {
+    for (var i = 0; i < tables.length; i++) {
+        makeTableFilterable(tables[i]);
+    }
+};
+
+function makeTableFilterable(table) {
+    var inputNode = document.createElement("input");
+    inputNode.type = "text";
+    table.parentNode.insertBefore(inputNode, table);   // 添加输入框
+    inputNode.oninput = function(event) {              // 为输入框声明过滤函数
+        filterFunc(event, table);
+    };
+};
+
+/**
+ * filter function
+ */
+function filterFunc(e, table) {
+    var value = e.target.value;
+    var rows = table.tBodies[0].rows;
+    for (var i = 0; i < rows.length; i++) {      // 令表格的每一行可见
+        rows[i].style.display = 'table-row';
+    }
+
+    var isFound = false;
+    for (var i = 0; i < rows.length; i++) {
+        isFound = false;
+        for (var j = 0; j < rows[i].cells.length; j++) {
+            var text = rows[i].cells[j].innerText;    // 获取innerTExt, 去除添加的样式
+            var position = text.indexOf(value);
+            if (position >= 0) {
+                var style = "<span class='highLight'>"+value+"</span>";    // 修改样式
+                var html = text.replace(value, style);
+                rows[i].cells[j].innerHTML = html;
+                isFound = true;
+            } else {
+                rows[i].cells[j].innerHTML = text;
+            }
+        }
+        if (!isFound) {   //如果无法匹配，将本行设置为不可见
+            rows[i].style.display = "none";
+        }
+    }
+};
