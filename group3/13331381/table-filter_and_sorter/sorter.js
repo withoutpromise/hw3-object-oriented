c6751ca63476d11ca02ca30a21055a93c8397ff2
朱术煌 <zsh5318@gmail.com>
2015-03-21 21:11:33 +0800

    update

--- group3/13331381/table-filter_and_sorter/sorter.js
+++ group3/13331381/table-filter_and_sorter/sorter.js
@@ -8,51 +8,84 @@ function makeAllTablesSortable(tables) {
 
 	// make a table sortable
 	function makeTableSortable(table) {
-		var theads = table.rows[0].cells;
-		for (var i = 0; i < theads.length; i++) {
-			theads[i].onclick = function (a, b, c) {
+		var thds = table.rows[0].cells;
+		for (var i = 0; i < thds.length; i++) {
+			thds[i].onclick = function (a, b, c) {
 				return function () {
 					changeClassName(a, b);
 					sortTable(a, b, c);
 				}
-			}(table, theads[i], i);
+			}(table, thds[i], i);
 		}
 
 		// sort table by given the table, thead, and column
-		function sortTable(table, obj, index) {
-			var rows = table.rows;
-			for (var i = 1; i < rows.length - 1; i++) {
-				for (var j = i+1; j < rows.length; j++) {
-					if (rows[i].cells[index].innerHTML < rows[j].cells[index].innerHTML) {
-						var t = rows[i].innerHTML;
-						rows[i].innerHTML = rows[j].innerHTML;
-						rows[j].innerHTML = t;
+		function sortTable(table, thd, index) {
+			var tbody = table.getElementsByTagName("tbody")[0];
+			var trs = tbody.getElementsByTagName("tr");
+			var match = document.getElementsByClassName("match");
+			//alert(match.length);
+
+			for (var i = 0; i < trs.length; i++) {
+				if (match.length <= 0 || trs[i].className.search("match") >= 0) {
+					for (var j = i+1; j < trs.length; j++) {
+						if (match.length <= 0 || trs[j].className.search("match") >= 0) {
+							if (trs[i].cells[index].innerHTML < trs[j].cells[index].innerHTML) {
+								if (match.length > 0) {
+									var classname = trs[i].className;
+									trs[i].className = trs[j].className;
+									trs[j].className = classname;
+									//alert(classname);
+								}
+
+								var t = trs[i].innerHTML;
+								trs[i].innerHTML = trs[j].innerHTML;
+								trs[j].innerHTML = t;
+							}
+						}
 					}
 				}
 			}
 
 			// reverse case
-			if (obj.className == "descend") {
-				var l = parseInt(rows.length / 2 + 1);
-				for (var i = 1; i < l; i++) {
-					var t = rows[i].innerHTML;
-					rows[i].innerHTML = rows[rows.length - i].innerHTML;
-					rows[rows.length - i].innerHTML = t;
+			if (thd.className == "descend") {
+				if (match.length <= 0) {
+					var l = parseInt(trs.length / 2 + 1);
+					for (var i = 0; i < l; i++) {
+						var t = trs[i].innerHTML;
+						trs[i].innerHTML = trs[trs.length-i-1].innerHTML;
+						trs[trs.length-i-1].innerHTML = t;
+					}
+				} else {
+					var i = 0,
+						j = trs.length - 1;
+					while (i < j) {
+						while (i < j && (trs[i].className.search("match") == -1)) i++;
+						while (i < j && (trs[j].className.search("match") == -1)) j--;
+						if (i < j) {
+							var classname = trs[i].className;
+							trs[i].className = trs[j].className;
+							trs[j].className = classname;
+
+							var t = trs[i].innerHTML;
+							trs[i++].innerHTML = trs[j].innerHTML;
+							trs[j--].innerHTML = t;
+						}
+					}
 				}
 			}
 		}
 
 		// change className
-		function changeClassName(table, obj) {
-			var ths = table.getElementsByTagName("th");
-			for (var i = 0; i < ths.length; i++) {
-				if (ths[i] != obj)
-					ths[i].className = "no_active";
+		function changeClassName(table, thd) {
+			var thds = table.getElementsByTagName("th");
+			for (var i = 0; i < thds.length; i++) {
+				if (thds[i] != thd)
+					thds[i].className = "no_active";
 			}
-			if (obj.className == "ascend") {
-				obj.className = "descend";
+			if (thd.className == "ascend") {
+				thd.className = "descend";
 			} else {
-				obj.className = "ascend";
+				thd.className = "ascend";
 			}
 		}
 	}
