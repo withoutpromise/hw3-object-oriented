e628a981061f8e33ffac07d36a68c7cb2b945b08
XvarX <yexj3@mail2.sysu.edu.cn>
2015-03-21 16:01:13 +0800

    13331315
    upload 13331315

---
+++ group1(afternoon)/13331315/first_problem/first.js
@@ -0,0 +1,56 @@
+//到现在还是搞不清楚
+function Base(variable)
+{
+    this.instanceVariable = variable;
+    this.instanceMethod = function()
+    {
+        console.log("This is from Base class instance-method, instance-variable is: " + this.instanceVariable);
+    };
+}
+
+Base.staticVariable = "Base";
+
+Base.staticMethod = function()
+{
+    console.log("This is from Base class static-method, static-variable is : " + this.staticVariable);
+};
+
+function Derived(variable)
+{
+    this.instanceVariable = variable;
+    this.instanceMethod = function()
+    {
+    	var base = new Base();
+    	this.__proto__.instanceMethod.call(this);//to use the Base function.
+        console.log("This is from Derived class instance-method, instance-variable is: " + this.instanceVariable);
+    };
+}
+
+Derived.staticVariable = "Derived";
+
+Derived.staticMethod = function()
+{
+    Derived.__proto__.staticMethod.call(this);//to use the Base function.
+    console.log("This is from Derived class static-method, static-variable is : " + this.staticVariable);
+};
+
+function extend(base, derived)
+{
+    derived.prototype = new base();
+    derived.__proto__ = base;//clone base to derived.__proto__ to use 
+    //但好像变成了克隆而不是继承写法了
+}
+
+extend(Base, Derived);
+
+console.log("First--------------------------");
+example = new Derived('example');
+Derived.staticMethod();
+example.instanceMethod();
+
+console.log("Second-------------------------");
+example = new Derived('example');
+otherExample = new Derived('other-example');
+Derived.staticMethod();
+example.instanceMethod();
+otherExample.instanceMethod();
