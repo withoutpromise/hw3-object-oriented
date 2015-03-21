0ec8def5375166e11964d2c1a715a3abed2786a8
unknown <daixuan1996@163.com>
2015-03-21 14:01:51 +0800

    part3 done and fix bugs

--- group4/13331043/3/filter.js
+++ group4/13331043/3/filter.js
@@ -32,8 +32,6 @@ function filterRows(table) {
         var target = this.value;
         var trs = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
         for (var tr = 0; tr < trs.length; ++tr) {
-            if (trs[tr].getElementsByTagName('th').length > 0) continue;
-
             var cells = trs[tr].getElementsByTagName('td');
             var flag = false;
             for (var td = 0; td < cells.length; ++td) {
@@ -48,7 +46,7 @@ function filterRows(table) {
                 }
             }
             if (!flag) {
-                trs[tr].style.display = 'none';
+                table.getElementsByTagName('tbody')[0].deleteRow(tr);
             }
         }
     };
---
+++ group4/13331043/README.md
@@ -0,0 +1,10 @@
+#### Author: Xuan Dai
+#### private repo: https://github.com/daix6/SPI/tree/master/lab3
+
+----
+
+##### 2,3 可在以下链接运行
+
+- [Sicily Online Judge](http://soj.sysu.edu.cn/courses.php)
+- [Lab 01. 环形菜单 - SE-386 Software Process Improvement - SoYa Wiki](http://my.ss.sysu.edu.cn/wiki/pages/viewpage.action?pageId=395968523)
+- [HTML DOM Audio 对象](http://www.w3school.com.cn/jsref/dom_obj_audio.asp)
